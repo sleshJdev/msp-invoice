372222588d13f93f3347d6d9d09ebb1aded15a93
slesh <slesh.eugene93@gmail.com>
2021-03-04 22:48:26 +0300

    few more method to interact with kaseya api

--- kaseya/kaseya.js
+++ kaseya/kaseya.js
@@ -29,32 +29,57 @@ const claim = Buffer.from(
     + ',rand2=' + random
 ).toString('base64')
 
-const get = (path, headers) => {
+const justGet = (path, headers) => {
     return new Promise((resolve, reject) => {
         http.get(`https://sc1.seamless-it.com/api/v1.0${path}`, {
             headers
         }, (response) => {
             let data = '';
             response.on('data', chunk => (data += chunk))
-            response.on('end', () => resolve(JSON.parse(data)))
-            response.on('error', (err) => {
-                reject(err)
-            })
+            response.on('end', () => (resolve(JSON.parse(data))))
+            response.on('error', (err) => (reject(err)))
         })
     })
 }
 
+const get = (() => {
+    let effectiveAuthInfo = null
+    return (path, headers) => {
+        const request = (retried, effectiveHeaders) => (
+            justGet(path, effectiveHeaders).then(res => {
+                if (!effectiveAuthInfo || (res.ResponseCode === 401 && !retried)) {
+                    return requestToken().then(authInfo => {
+                        effectiveAuthInfo = authInfo
+                        return request(true, {
+                            ...effectiveHeaders,
+                            Authorization: `Bearer ${effectiveAuthInfo.Result.ApiToken}`
+                        })
+                    })
+                }
+                return res
+            })
+        )
+        if (effectiveAuthInfo) {
+            return request(false, {
+                ...headers,
+                Authorization: `Bearer ${effectiveAuthInfo.Result.ApiToken}`
+            }).then(res => {
+                return res.Result
+            })
+        }
+        return request(false, headers).then(res => {
+            return res.Result
+        })
+    }
+})()
+
 const requestToken = async () => {
-    return get('/auth', {
+    return justGet('/auth', {
         Authorization: `Basic ${claim}`
     })
 }
 
-const fetchProcs = async (token) => {
-    return get('/automation/agentprocs', {
-        Authorization: `Bearer ${token}`
-    })
-}
+const fetchProcs = async () => (get('/automation/agentprocs'))
 
 const fetchMachineGroups = (token, orgId) => {
     return get(`/system/orgs/${orgId}/machinegroups`, {
@@ -68,11 +93,10 @@ const fetchOrganizations = (token) => {
     })
 }
 
-const fetchAuditSummaries = (token) => {
-    return get('/assetmgmt/audit', {
-        Authorization: `Bearer ${token}`
-    })
-}
+const fetchPatchStatus = (agentId) => (get(`/assetmgmt/patch/${agentId}/status`))
+const fetchAuditSummaries = () => (get('/assetmgmt/audit'))
+const fetchSecurityProducts = (agentId) => (get(`/assetmgmt/audit/${agentId}/software/securityproducts`))
+const fetchPatchHistory = (agentId) => (get(`/assetmgmt/patch/${agentId}/history`))
 
 const fetchUser = (token, userId) => {
     return get(`/system/users/${userId}`, {
@@ -81,9 +105,22 @@ const fetchUser = (token, userId) => {
 }
 
 (async () => {
-    const authInfo = await requestToken()
-    const { Result: { ApiToken, Token, AdminId } } = authInfo
-    const summaries = await fetchAuditSummaries(ApiToken)
-    const procs = await fetchProcs(ApiToken)
-    console.log(JSON.stringify(procs, null, 2))
+    //const authInfo = await requestToken()
+    //const { Result: { ApiToken, Token, AdminId } } = authInfo
+    const summaries = await fetchAuditSummaries()
+    for (let summary of summaries) {
+        const patchStatuses = await fetchPatchStatus(summary.AgentGuid)    
+        const installedSecurityProducts = await fetchSecurityProducts(summary.AgentGuid)
+        const patchHistory = await fetchPatchHistory(summary.AgentGuid)
+        console.log(`Agent name: ${summary.DisplayName}, 
+            patchStatuses: ${JSON.stringify(patchStatuses, null, 2)}, 
+            patchHistory: ${JSON.stringify(patchHistory, null, 2)}`)
+    }
+    const patchStatuses = await fetchPatchStatus(952862034213546)
+    // console.log(summaries.map(it => (it.DisplayName)))
+    // console.log(summaries.map(it => (it.DisplayName)).filter(it => it.toLowerCase().includes('server')))
+    // const procs = await fetchProcs()
+    // console.log(JSON.stringify(procs, null, 2))
+
+    // console.log(JSON.stringify(summaries, null, 2))
 })()
