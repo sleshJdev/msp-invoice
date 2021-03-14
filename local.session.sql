b8b2eb61ab23414110b2c29cb5cf5c5c8983f185
slesh <slesh.eugene93@gmail.com>
2021-03-14 13:56:05 +0300

    Updated clickhouse DDL

--- local.session.sql
+++ local.session.sql
@@ -1,2 +1,4 @@
-CREATE TABLE agents (AgentId UInt64, AgentName String, Online UInt8, OSInfo String, MachineGroup String) ENGINE=URL('http://kaseya-proxy:8090/api/v1.0/assetmgmt/agents?accept=jsonl', JSONEachRow) SETTINGS input_format_skip_unknown_fields=1;
-CREATE TABLE pathHistory (extAgentId UInt64, UpdateTitle String, PatchState UInt8) ENGINE=URL('http://kaseya-proxy:8090/assetmgmt/patch/history?accept=jsonl', JSONEachRow) SETTINGS input_format_skip_unknown_fields=1;
+-- CREATE TABLE view_agents (AgentId UInt64, AgentName String, Online UInt8, OSInfo String, MachineGroup String) ENGINE=URL('http://kaseya-proxy:8090/api/v1.0/assetmgmt/agents?accept=jsonl', JSONEachRow) SETTINGS input_format_skip_unknown_fields=1;
+-- CREATE TABLE view_patch_history (AgentId UInt64, UpdateTitle String, PatchState UInt8) ENGINE=URL('http://kaseya-proxy:8090/assetmgmt/patch/history?accept=jsonl', JSONEachRow) SETTINGS input_format_skip_unknown_fields=1;
+-- drop table agents;
+-- drop table pathHistory;
